bcdb477e38b5f014c601638518fe36ce873370d4
Grig <chiefy@mail.ru>
2022-09-08 10:51:39 +0300

    Add Cycle In Cycle ezample

--- CycleInCycle/Program.cs
+++ CycleInCycle/Program.cs
@@ -1,14 +1,22 @@
-﻿string Method4(int count, string text)
-{ 
-    int i = 0;
-    string result = String.Empty; 
+﻿// string Method4(int count, string text) // переписываем метод4 для цикла "for" 
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "z");
+//Console.WriteLine(res);
 
-    while (i < count)
+// Cycle In cycle - вывод таблицы умножения на экран
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
     {
-        result = result + text;
-        i++;
+        Console.WriteLine($"{i} x {j} = {i*j}"); // интерполяция строк
     }
-    return result;
+    Console.WriteLine();
 }
-string res = Method4(10, "z");
-Console.WriteLine(res);
